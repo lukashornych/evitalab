f229564c846f5ad3f9a6a2497ae0c1fc9ab57579
Lukáš Hornych <lukashornych@proton.me>
2023-08-27 21:32:58 +0200

    feat: support for running lab from evitaDB directly

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/',
+    path: '/lab',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
