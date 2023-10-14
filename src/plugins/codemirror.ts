4f774ceea08f8c7f56f7cedcac4ba038946fefae
LHO <hornych@fg.cz>
2023-10-14 10:05:55 +0200

    style(#48): fix issue with multiple codemirror themes used concurrently, better dracula theme

--- src/plugins/codemirror.ts
+++ src/plugins/codemirror.ts
@@ -1,12 +1,9 @@
 import VueCodemirror from 'vue-codemirror'
-import {dracula} from 'thememirror';
 
 // https://thememirror.net/
 
 export const codemirror = VueCodemirror
 
 export const defaultCodemirrorOptions =  {
-    extensions: [
-        dracula
-    ]
+    extensions: []
 }
