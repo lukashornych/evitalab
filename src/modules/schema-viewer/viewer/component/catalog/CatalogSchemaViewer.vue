e8c831b06ec06e871f5a4c4336a2a3193422dcb1
Ondřej Mikolášek <mikolasek.ondrej@ssakhk.cz>
2024-07-16 12:07:55 +0200

    refactor: fixed formatting

--- src/modules/schema-viewer/viewer/component/catalog/CatalogSchemaViewer.vue
+++ src/modules/schema-viewer/viewer/component/catalog/CatalogSchemaViewer.vue
@@ -1,55 +1,54 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { useI18n } from "vue-i18n";
-import { Catalog } from "@/modules/connection/model/Catalog";
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
 import {
     ConnectionService,
     useConnectionService,
-} from "@/modules/connection/service/ConnectionService";
-import { SchemaViewerDataPointer } from "@/modules/schema-viewer/viewer/model/SchemaViewerDataPointer";
-import { CatalogSchema } from "@/modules/connection/model/schema/CatalogSchema";
-import { Property } from "@/modules/base/model/properties-table/Property";
-import { PropertyValue } from "@/modules/base/model/properties-table/PropertyValue";
-import SchemaContainer from "@/modules/schema-viewer/viewer/component/SchemaContainer.vue";
-import NameVariants from "@/modules/schema-viewer/viewer/component/NameVariants.vue";
-import AttributeSchemaList from "@/modules/schema-viewer/viewer/component/attribute/AttributeSchemaList.vue";
-import EntitySchemaList from "@/modules/schema-viewer/viewer/component/entity/EntitySchemaList.vue";
-import { List } from "immutable";
-import { Toaster, useToaster } from "@/modules/notification/service/Toaster";
+} from '@/modules/connection/service/ConnectionService'
+import { SchemaViewerDataPointer } from '@/modules/schema-viewer/viewer/model/SchemaViewerDataPointer'
+import { CatalogSchema } from '@/modules/connection/model/schema/CatalogSchema'
+import { Property } from '@/modules/base/model/properties-table/Property'
+import { PropertyValue } from '@/modules/base/model/properties-table/PropertyValue'
+import SchemaContainer from '@/modules/schema-viewer/viewer/component/SchemaContainer.vue'
+import NameVariants from '@/modules/schema-viewer/viewer/component/NameVariants.vue'
+import AttributeSchemaList from '@/modules/schema-viewer/viewer/component/attribute/AttributeSchemaList.vue'
+import EntitySchemaList from '@/modules/schema-viewer/viewer/component/entity/EntitySchemaList.vue'
+import { List } from 'immutable'
+import { Toaster, useToaster } from '@/modules/notification/service/Toaster'
 
-const { t } = useI18n();
+const { t } = useI18n()
 
 const props = defineProps<{
-    dataPointer: SchemaViewerDataPointer;
-    schema: CatalogSchema;
-}>();
+    dataPointer: SchemaViewerDataPointer
+    schema: CatalogSchema
+}>()
 
-const catalogId = ref<string>();
-const loaded = ref<boolean>(false);
-const toaster: Toaster = useToaster();
-const connectionService: ConnectionService = useConnectionService();
+const catalogId = ref<string>()
+const loaded = ref<boolean>(false)
+const toaster: Toaster = useToaster()
+const connectionService: ConnectionService = useConnectionService()
 connectionService
     .getCatalog(props.dataPointer.connection, props.schema.name)
     .then((x) => {
-        catalogId.value = x.catalogId.getIfSupported();
-        loaded.value = true;
+        catalogId.value = x.catalogId.getIfSupported()
+        loaded.value = true
     })
-    .catch((e) => toaster.error(e));
+    .catch((e) => toaster.error(e))
 
 const baseProperties = ref<Property[]>([
     {
-        name: t("schemaViewer.catalog.label.catalogId"),
+        name: t('schemaViewer.catalog.label.catalogId'),
         value: new PropertyValue(catalogId),
     },
     {
-        name: t("schemaViewer.catalog.label.version"),
+        name: t('schemaViewer.catalog.label.version'),
         value: new PropertyValue(props.schema.version.getIfSupported()),
     },
     {
-        name: t("schemaViewer.catalog.label.description"),
+        name: t('schemaViewer.catalog.label.description'),
         value: new PropertyValue(props.schema.description.getIfSupported()!),
     },
-]);
+])
 </script>
 
 <template>
