cd7cf27d3c42fc13b60f7f8e4bb93151306d7a06
Lukáš Hornych <lukashornych@proton.me>
2024-09-01 16:56:47 +0200

    fix: representative flags cannot be computed due to the readonly proxy

--- src/modules/schema-viewer/viewer/component/attribute/AttributeSchemaListItem.vue
+++ src/modules/schema-viewer/viewer/component/attribute/AttributeSchemaListItem.vue
@@ -16,6 +16,7 @@ import { ReferenceSchemaPointer } from '@/modules/schema-viewer/viewer/model/Ref
 import { ReferenceAttributeSchemaPointer } from '@/modules/schema-viewer/viewer/model/ReferenceAttributeSchemaPointer'
 import { UnexpectedError } from '@/modules/base/exception/UnexpectedError'
 import SchemaContainerSectionListItem from '@/modules/schema-viewer/viewer/component/SchemaContainerSectionListItem.vue'
+import { computed, ComputedRef } from 'vue'
 
 const workspaceService: WorkspaceService = useWorkspaceService()
 const schemaViewerTabFactory: SchemaViewerTabFactory = useSchemaViewerTabFactory()
@@ -25,7 +26,7 @@ const props = defineProps<{
     schema: AttributeSchema
 }>()
 
-const flags: List<string> = props.schema.getRepresentativeFlags()
+const flags: ComputedRef<List<string>> = computed(() => props.schema.representativeFlags)
 
 function openAttributeSchema(): void {
     const parentSchemaPointer = props.dataPointer.schemaPointer
